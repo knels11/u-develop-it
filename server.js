1e94cc2b3bb552e38564dac851fbb1ff7ef7eb49
kiehp <kierra.nelson@aol.com>
2021-10-31 16:41:39 -0400

    built db call

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,7 +7,23 @@ const app = express();
 app.use(express.urlencoded({  extended: false  }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'spicyTuna24wks!',
+    database: 'election'
+},
+console.log('Connected to election database.')
+);
 
+//return all data in the candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+
+//catchall route
 //handle user requests not supported by the app (Not Found)
 app.use((req, res) => {
     res.status(404).end();
