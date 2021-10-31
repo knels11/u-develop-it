a16958bd13e627c39ab5d3c0984e170277d16f16
kiehp <kierra.nelson@aol.com>
2021-10-31 17:11:28 -0400

    CRUD routes in plaace

--- server.js
+++ server.js
@@ -17,11 +17,37 @@ const db = mysql.createConnection({
 console.log('Connected to election database.')
 );
 
-//return all data in the candidates table
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+//GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    //if (err) {
+        //console.log(error)
+    //}
+    //console.log(row);
+//});
+//Delete a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+    //if (err) {
+        //console.log(err);
+    //}
+    //console.log(results);
+//});
 
+//create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//VALUES (?,?,?,?)`;
+
+//const params = [1, 'Ronald', 'Firbank', 1];
+//db.query(sql, params, (err, result) => {
+    //if (err) {
+        //console.log(err);
+    //}
+    //console.log(result);
+//});
+
+//return all data in the candidates table
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+//});
 
 //catchall route
 //handle user requests not supported by the app (Not Found)
